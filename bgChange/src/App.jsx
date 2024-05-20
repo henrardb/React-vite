e4354a8712b9f6132d19784f756f4b9dc70323c7
henrardb <bruno.henrard@gmail.com>
2024-05-20 16:47:59 +0200

    simple background change

--- bgChange/src/App.jsx
+++ bgChange/src/App.jsx
@@ -1,15 +1,39 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("olive");
 
   return (
-    <>
-      <h1 className="bg-green-500 text-3xl">bgChange App with Vite</h1>
-    </>
+    <div
+      className="w-full h-screen duration-200 bg-black"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-full">
+          <button
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full"
+            onClick={() => setColor("red")}
+          >
+            Red
+          </button>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+            onClick={() => setColor("blue")}
+          >
+            Blue
+          </button>
+          <button
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
+            onClick={() => setColor("green")}
+          >
+            Green
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
